fe6d77bf61399dfc8b38da43f29469b60fa13e39
shoam <shoam611@gmail.com>
2022-02-14 02:40:32 +0200

    added fetch demo function

--- test-sys-client/src/Store/actions/question.js
+++ test-sys-client/src/Store/actions/question.js
@@ -2,8 +2,18 @@ export const ADD = 'ADD';
 export const FETCH = 'FETCH';
 export const DELETE = 'DELETE';
 
-export const addQuestion= (newQuestion) => {
-    return (dispatch)=>{
-        dispatch({type:ADD,newQuestion})
+export const addQuestion = (newQuestion) => {
+    return async (dispatch, getState) => {
+        //can use axios here to push a product to the server
+        const response = await fetch(
+            'url from .env',
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'aplication/json'},
+                body: JSON.stringify(newQuestion)
+            }
+        );
+        const responseData = await response.json();
+        dispatch({ type: ADD, newQuestion })
     }
 }
