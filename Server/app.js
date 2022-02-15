d144d6bd85b7f9f98b00ad985f1546738ceb86e7
shoam <shoam611@gmail.com>
2022-02-15 13:11:25 +0200

    url suffixes depracated

--- Server/app.js
+++ Server/app.js
@@ -5,8 +5,8 @@ const {container} = require('./app-container');
 
 app.post('/',async (req,res)=> { res.send(200) });
 const questionsController = container.resolve('questionsController');
-app.post('/questions/post',async (req,res)=> {questionsController.addQuestion(req); res.send(200)});
-app.get('/questions/get',async (req,res)=> {questionsController.getQuestions(req); res.send(200)});
-app.delete('/questions/delete',async (req,res)=> {questionsController.deleteQuestion(req); res.send(200)});
+app.post('/questions',async (req,res)=> {questionsController.addQuestion(req); res.send(200)});
+app.get('/questions',async (req,res)=> {questionsController.getQuestions(req); res.send(200)});
+app.delete('/questions',async (req,res)=> {questionsController.deleteQuestion(req); res.send(200)});
 
 module.exports = app
