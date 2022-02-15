f068231c9d727f3e3a44d5b40c75d1017f737b1f
shoam <shoam611@gmail.com>
2022-02-15 02:50:30 +0200

    option c works sync

--- Server/data/mongoRepository.js
+++ Server/data/mongoRepository.js
@@ -22,27 +22,44 @@ class MongoRepository {
             [Symbol.asyncIterator]: async function* () {
                 for (let doc = await query.next(); doc != null; doc = await query.next())
                     yield doc;
-            }   }
+            }
+        }
     }
+    *createIterator(query) {
+        for (let doc = query.next(); doc != null; doc = query.next())
+            yield doc;
+
+    }
+
+
     async getAllAsync() {
         console.log('in get all async');
-        const query = this.QuestionModel.find().skip(2).limit(4).cursor();
-        //-----------------------------
+        const query = this.QuestionModel.find().skip(2).limit(5).cursor();
+        //----------------------------- option A
         // for (let i=1,doc = await query.next(); doc != null; doc = await query.next()) {
         //     console.log(`**********start ${i++}************`);
         //     console.log(doc);
         //     console.log(`**********end  ${i++}************`);
         // }
+        //-----------------------------option B
+        // const asynciterable = this.createIteratorAsync(query)
+        // for await (const document of asynciterable) {
+        // console.log('**********start************');
+        // console.log(document);
+        // console.log('**********end************');
+        // }
+        //-----------------------------option C
+        // const iterable = this.createIterator(query);
+        // const sequence = Enumerable.from(iterable);
+        // let i=1;
+        // for (const document of sequence) {
+        //     if(! await document) break;
+        //     console.log(`********* start ${i}************`);
+        //     console.log(await document);
+        //     console.log(`********** end ${i++}*************`);
+        // }
         //-----------------------------
-        const iterable = this.createIteratorAsync(query)
-        const sequence = Enumerable.from(iterable);
-
-        for await (const document of sequence) {
-            console.log('**********start************');
-            console.log(document);
-            console.log('**********end************');
-            if (!document) break;
-        }
+        // return iterable
     }
 
 }
