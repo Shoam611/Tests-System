815a8974a2c2f9fb5c4e199cb6450d85998d8a01
Liad Dadon <80317730+liaddx1@users.noreply.github.com>
2022-02-17 14:08:45 +0200

    Validation on submit for Question creation

--- test-sys-client/src/views/question-form/createQuestionForm.js
+++ test-sys-client/src/views/question-form/createQuestionForm.js
@@ -6,10 +6,11 @@ import AwnsersSelector from './answerSelector';
 import AwnserChoice from './answerChoice';
 import './createQuestionForm.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { addQuestion,fetchQuestions } from 'Store/actions/question';
+import { addQuestion, fetchQuestions } from 'Store/actions/question';
 const CreateQuestionForm = () => {
     //helpers
     const [ignored, forceUpdate] = useReducer(x => x + 1, 0);
+
     const getId = () => {
         if (answers.length > 0) {
             return answers.at(-1).id + 1;
@@ -19,38 +20,83 @@ const CreateQuestionForm = () => {
     const dispatch = useDispatch();
     //handlers
     const handleQuestionTypeChanged = (qType) => {
-        answers.forEach(item=>{item.isSelected=false;item.value=''})
+        answers.forEach(item => { item.isSelected = false; item.value = '' })
         setQuestionType(qType);
     }
     const onAddingAwnser = () => {
         if (answers.length >= 6) return;
         const id = getId();
-        const newAnswer = { id: id, render: <AwnserChoice id={id} onRemove={handleRemoveAwnser} onChange={awnserContentChangedHandler}  />, value: '' ,isSelected:false};
+        const newAnswer = { id: id, render: <AwnserChoice id={id} onRemove={handleRemoveAwnser} onChange={awnserContentChangedHandler} />, value: '', isSelected: false };
         answers.push(newAnswer);
         forceUpdate();
     }
     const handleRemoveAwnser = (id) => {
         if (answers.length > 2) {
             const index = answers.indexOf(answers.find(i => i.id === id));
-            if (index >= 0) { answers.splice(index, 1);  }
+            if (index >= 0) { answers.splice(index, 1); }
             forceUpdate()
         }
     }
     const awnserContentChangedHandler = (value, id) => {
-        console.log('changed');
         const temp = answers;
         temp.filter(i => i.id === id)[0].value = value;
         setAwnsers(temp);
     }
+
+    const formValidation = () => {
+        if (+questionType < 1 || +questionType > 3) {
+            setErrorMessage('Please Choose a Question Types.');
+            return false;
+        }
+        if (!topic) {
+            setErrorMessage('Please Choose a Topic.');
+            return false;
+        }
+        if (Question_text.value.trim().length === 0) {
+            setErrorMessage('Question Text Cannot be Empty.');
+            return false;
+        }
+        if (Text_above_question.value.trim().length === 0) {
+            setErrorMessage('Text Above Question Be Empty.');
+            return false;
+        }
+        if (Text_below_question.value.trim().length === 0) {
+            setErrorMessage('Text Below Question Be Empty.');
+            return false;
+        }
+        if (tags.value.trim().length === 0) {
+            setErrorMessage('You Must Enter At Least One Tag.');
+            return false;
+        }
+        for (let answer of answers) {
+            if (answer.value.trim().length === 0) {
+                setErrorMessage('Answers Cannot Be Empty.');
+                return false;
+            }
+        }
+        if (getIndexes(answers).length === 0) {
+            setErrorMessage('You Must Choose Right Answer/s.');
+            return false;
+        }
+
+        setErrorMessage('');
+        return true;
+    }
+
+    const getIndexes = (answers) => {
+        return answers.map(({ isSelected }, index) => ({ isCorrect: isSelected, index })).filter(({ isCorrect }) => isCorrect).map(({ index }) => index);
+    }
     const handleSubmit = (e) => {
         e.preventDefault();
-        const newQuestion = new Question(topic, questionType, Question_text.value, Text_above_question.value, Text_below_question.value, tags.value, answers.map(({value},index) => ({value,id:index})) ,answers.map(({isSelected},index)=> ({isCorrect:isSelected,index})).filter(({isCorrect})=>isCorrect).map(({index})=>index));
-        console.log(newQuestion);
-        dispatch(addQuestion(newQuestion))
+        if (formValidation()) {
+            const newQuestion = new Question(topic, questionType, Question_text.value, Text_above_question.value, Text_below_question.value, tags.value, answers.map(({ value }, index) => ({ value, id: index })), getIndexes(answers));
+            console.log(newQuestion);
+            dispatch(addQuestion(newQuestion))
+        }
     }
-    
+
     const questions = useSelector(state => state.questions.questions)
-    const printToConsole=()=>{
+    const printToConsole = () => {
         console.log(questions);
     }
     //states
@@ -58,12 +104,14 @@ const CreateQuestionForm = () => {
     const [topic, setTopic] = useState('');
     const [questionType, setQuestionType] = useState(null);     //selected q type
     const [answers, setAwnsers] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
     //side-effects
     useEffect(() => {
         onAddingAwnser(); onAddingAwnser();
         setQuestionTypes([{ id: 1, value: 'Single choice' }, { id: 2, value: 'Multi Choice' }]);
         setTopic('def-topic');
-    },[]);
+    }, []);
     //inputs
     const Question_text = useInput();
     const Text_above_question = useInput();
@@ -72,7 +120,7 @@ const CreateQuestionForm = () => {
     //renderers
     return (
         <div className='AddQForm'>
-            <h1>New Question</h1> 
+            <h1>New Question</h1>
             <Btn onClick={printToConsole}>print to console</Btn>
             <div style={{ height: 'min-content' }}>
                 <form onSubmit={handleSubmit} >
@@ -84,10 +132,11 @@ const CreateQuestionForm = () => {
                         <Input placeholder="Text below question:"  {...Text_below_question} />
                         <Input placeholder="tags (seperate with , charecter)" {...tags} />
                         <hr />
-                        {questionType && <AwnsersSelector   onAddingAwnser={onAddingAwnser}
-                                                            list={answers}
-                                                            questionType={questionType} />}
+                        {questionType && <AwnsersSelector onAddingAwnser={onAddingAwnser}
+                            list={answers}
+                            questionType={questionType} />}
                         <Input type="submit" value="Submit" />
+                        <p className='ErrorMessage'>{errorMessage}</p>
                     </div>
                 </form>
             </div>
