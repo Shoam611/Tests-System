c43a2c6ea8b1b8f6f673fc007c84c33759cddd2a
shoam <shoam611@gmail.com>
2022-02-24 17:52:11 +0200

    question edit view update

--- test-sys-client/src/views/manageQuestionsView.js
+++ test-sys-client/src/views/manageQuestionsView.js
@@ -1,25 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { Box, Btn, Icon, Line } from 'UIKit';
+import { Box, Btn, Icon, Input, Line } from 'UIKit';
 import './manageQuestionsView.css'
 const ManageQuestionView = (props) => {
-    const topic = useSelector(state=>state.topic.topic);
-    const [questionType] = useState([{id:1,name:'Single choice'},{id:1,name:'Multi choice'}]);
+    const topic = useSelector(state => state.topic.topic);
+    const [questionType] = useState([{ id: 1, name: 'Single choice' }, { id: 1, name: 'Multi choice' }]);
     const questions = useSelector(state => state.questions.questions)
 
     const handleShowPrev = () => { console.log(questions); }
     const handleShownext = () => { }
 
     const renderQuestions = () => {
-        return ( questions.length===0 ? <h2>No Questions available for the selected topic : {topic.name}</h2>:
+        return (questions.length === 0 ? <h2>No Questions available for the selected topic : {topic.name}</h2> :
             questions.map((q) => {
                 return (
                     <Box key={q._id} >
-                        <div className='questions-container grid'>
+                        <div className='questions-container-item'>
                             <Icon i={q.isAnActiveQuestion ? 'check' : 'times'} />
                             <h4> {q.questionText}</h4>
                             <h4> {new Date(q.updatedAt).toDateString()}</h4>
-                            <h4> {questionType.find(type=>type.id===q.questionType).name}</h4>
+                            <h4> {questionType.find(type => type.id === q.questionType).name}</h4>
                             <Btn i='caret-down'>Actions</Btn>
                         </div>
                     </Box>
@@ -28,34 +28,39 @@ const ManageQuestionView = (props) => {
     }
     return (
         <div className='mange-question-view'>
-            <h1>Questions for <span>{topic.name}</span></h1>
+            <h1>Questions for: <span>{topic.name}</span></h1>
+            <Line>
+                <div style={{ width: '250px' ,margin:'var(--gap-m) 0',padding:0}}>
+                    <Input placeholder="search by tags" />
+                </div>
+            </Line>
             <div className='question-wrapper'>
-
-                <div className='questions-container'>
-                    <Box >
-                        <div className='questions-container grid grid-header'>
-                            <h4 >Is active</h4>
-                            <h4> questionText</h4>
-                            <h4> updatedAt</h4>
-                            <h4> questionType</h4>
-                            <h4> actions</h4>
-                        </div>
-                    </Box>
+                    <div className='questions-container-item grid-header'>
+                        <Box>   <h4 >Is active</h4>     </Box>
+                        <Box>   <h4> questionText</h4>  </Box>
+                        <Box>   <h4> updatedAt</h4>     </Box>
+                        <Box>   <h4> questionType</h4>  </Box>
+                        <h4> actions</h4>
+                    </div>
+                <div className='questions-container '>
                     {renderQuestions()}
                 </div>
+
                 <Line justify="around">
                     <Line>
-                            <Box >
-                                <Line>
-                                    <Btn i="chevron-left" onClick={handleShowPrev} />
-                                    <h4>go forward / backwords</h4>
-                                    <Btn i="chevron-right" onClick={handleShownext} />
-                                </Line>
-                            </Box>
+                        <Box >
+                            <Line>
+                                <Btn i="chevron-left" onClick={handleShowPrev} />
+                                <h4>go forward / backwords</h4>
+                                <Btn i="chevron-right" onClick={handleShownext} />
+                            </Line>
+                        </Box>
                     </Line>
                 </Line>
             </div>
+        <div>
 
+        </div>
         </div>
     )
 }
