f136906128374b8200464d30cff1cff1bfa50730
shoam <shoam611@gmail.com>
2022-03-06 20:40:01 +0200

    connection established

--- Server/data/questionRepository.js
+++ Server/data/questionRepository.js
@@ -1,37 +1,40 @@
-const { Question } = require('./schemas/createConnection.js');
+const { getModels } = require('./schemas/createConnection.js');
 const { logger }= require('../app-logger.js')
 class MongoRepository {
     //Create
-    // async addAsync(object) {
-    //     try {
-    //         const q = new Question({...object});
-    //         await q.save();
-    //         return q._id;
-    //     } catch (err) {
-    //         const newErr = new Error(`error while trying to ad question to the db at q-repository. original error ${err.message}`)
-    //         logger.error(newErr);
-    //         return newErr
-    //     }
-    // }
-    // //Delete
-    // async DeleteOneAsync(id) {
-    //     try { Question.deleteByIdAsync(id); }
-    //     catch (err) { throw new Error(`error while trying to delete question ${id} from the db at q-repository. original error ${err.message}`); }
-    // }
+    async addAsync(object) {
+        const {Question} = getModels();
+        try {
+            const q = new Question({...object});
+            await q.save();
+            return q._id;
+        } catch (err) {
+            const newErr = new Error(`error while trying to ad question to the db at q-repository. original error ${err.message}`)
+            logger.error(newErr);
+            return newErr
+        }
+    }
+    //Delete
+    async DeleteOneAsync(id) {
+        const {Question} = getModels();
+        try { Question.deleteByIdAsync(id); }
+        catch (err) { throw new Error(`error while trying to delete question ${id} from the db at q-repository. original error ${err.message}`); }
+    }
     //Read
     async getAsync(filterquery = {}) {
-        // const { Question } = require('./schemas/createConnection.js');
+        const {Question} = getModels();
         try { return await Question.find({ sort: '-createdAt' }).where(filterquery); }
         catch (err) {
             const newErr = new Error(`error while trying to fetch questions from the db at q-repository. original error ${err.message}`); 
-             logger.log('error' ,newErr.message);
+             logger.error(newErr.message);
             throw newErr
         }
     }
     //Update
-    // async updateOneAsync(id, newQuestion) {
-    //     try { await Question.updateOne({ _id: id }, newQuestion);} 
-    //     catch (err) { throw new Error(`error while trying to update questions from the db at q-repository. original error ${err.message}`); }
-    // }
+    async updateOneAsync(id, newQuestion) {
+        const {Question} = getModels();
+        try { await Question.updateOne({ _id: id }, newQuestion);} 
+        catch (err) { throw new Error(`error while trying to update questions from the db at q-repository. original error ${err.message}`); }
+    }
 }
 module.exports = MongoRepository
--- Server/data/schemas/createConnection.js
+++ Server/data/schemas/createConnection.js
@@ -5,22 +5,23 @@ const createTopicModel = require('./topicSchema');
 const createUserModel = require('./userSchema');
 const { logger } = require('../../app-logger');
 const models = {};
+let Test, Topic, Question, User
 const createDataConnection = async () => {
     const { domain, db_port, database_data_name, database_users_name } = process.env;
-
     logger.info('attempts to establish connection to the data db server. at createDataConnection 11')
     createConnection(`mongodb://${domain}:${db_port}/${database_data_name}`).asPromise().then((con) => {
-        models.Question = createQuestionModel(con);
-        console.log('Question : ',   models.Question);
+        Question = createQuestionModel(con);
         Test = createTestModel(con);
         Topic = createTopicModel(con);
     }).catch((err) => { logger.error(`failed to connect to the db server. original error : ${err.message} at: createDataConnection 17`); })
     logger.info('established connection to the db server. at createDataConnection 16')
-    // try {
-    //     logger.info('attempts to establish connection to the user db server. at createDataConnection 19')
-    //     usersDb = await createConnection(`mongodb://${domain}:${db_port}/${database_users_name}`)
-    //     User = createUserModel(usersDb);
-    //     logger.log('info', 'established connection to the db user server. at createDataConnection 22')
-    // } catch (err) { logger.error(`failed to connect to the db user server. original error : ${err.message} at: createDataConnection 23`) }
+
+    logger.info('attempts to establish connection to the user db server. at createDataConnection 19')
+    createConnection(`mongodb://${domain}:${db_port}/${database_users_name}`).asPromise().then((con) => {
+        User = createUserModel(con);
+        logger.log('info', 'established connection to the db user server. at createDataConnection 22')
+    }).catch((err) => { logger.error(`failed to connect to the db user server. original error : ${err.message} at: createDataConnection 23`) })
 }
-module.exports = { createDataConnection, ...models }
+
+const getModels = () => ({ Test, Topic, Question,User });
+module.exports = { createDataConnection, Test, Topic, Question,User, getModels };
