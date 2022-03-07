f9b83183b95ffcca1c84944130815ee69bac7ddb
shoam <shoam611@gmail.com>
2022-03-07 14:16:59 +0200

    try catch

--- Server/routes/topic.routes.js
+++ Server/routes/topic.routes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const { container } = require('../app-container');
 const topicController = container.resolve('topicController');
-router.get('/', async (req, res) => { res.send(JSON.stringify(await topicController.getDefaultTopic())); });
+router.get('/', async (req, res) => {
+    try { es.send(JSON.stringify(await topicController.getDefaultTopic())); }
+    catch (err) { res.send(err.message).status(500); }
+});
 module.exports = router
--- Server/routes/users.routes.js
+++ Server/routes/users.routes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const { container } = require('../app-container');
 const usersController = container.resolve('usersController');
-router.post('/', async (req, res) => { usersController.addUser(req); res.send(200) });
+router.post('/', async (req, res) => {
+    try{   res.send(usersController.addUser(req)).status(200); 
+    } catch(err){res.send(err.message).status(500)}
+});
 router.get('/', async (req, res) => { const result = await usersController.getUsers(req); res.send(result).status(200) });
 router.delete('/', async (req, res) => { usersController.deleteUser(req); res.send(200) });
 router.put('/', async (req, res) => { const id = await usersController.updateUser(req); res.send(id.toString()).status(200); });
