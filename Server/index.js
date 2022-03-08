98ada563e58d53d5f4e00eb37efa974aa61ef3da
shoam <shoam611@gmail.com>
2022-03-08 21:37:23 +0200

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -14,3 +14,10 @@ require('./data/schemas/createConnection.js').createDataConnection();
 
 //server-startup
 app.listen(port, () => console.log(`Server is running on PORT: ${port}`));
+
+// PORT = 4200
+// domain = "localhost"
+// db_port = 27017
+// database_data_name = "quizDb"
+// database_records_name = "quizDbRecords"
+// database_users_name = "quizDbUsers"
