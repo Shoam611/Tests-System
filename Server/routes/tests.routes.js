974e21294e1684294b4bf317244fd1be6659d515
shoam <shoam611@gmail.com>
2022-03-08 22:17:44 +0200

    try catch routes

--- Server/routes/tests.routes.js
+++ Server/routes/tests.routes.js
@@ -6,8 +6,18 @@ router.post('/', async (req, res) => {
     try { res.send(await testsController.addTest(req)).status(200) }
     catch (err) { res.send(err.message).status(500) }
 });
-router.get('/', async (req, res) => { const result = await testsController.getTests(req); res.send(result).status(200) });
-router.delete('/', async (req, res) => { testsController.deleteTest(req); res.send(200) });
-router.put('/', async (req, res) => { const id = await testsController.updateTest(req); res.send(id.toString()).status(200); });
+router.get('/', async (req, res) => {
+    try { res.send(await testsController.getTests(req)).status(200) }
+    catch (err) { res.send(err.message).status(500) }
+});
+
+router.delete('/', async (req, res) => {
+    try { testsController.deleteTest(req); res.send(200) }
+    catch (err) { res.send(err.message).status(500) }
+});
+router.put('/', async (req, res) => {
+    try{res.send((await testsController.updateTest(req)).toString()).status(200);}
+    catch (err) { res.send(err.message).status(500) }
+});
 
 module.exports = router
